f7c4217a57a2d2b3a86f4c0bab0c78f2e9a43d7a
xGodai <Thomas_Brown_@Outlook.com>
2025-06-11 10:45:18 +0100

    Display Addition Question to the User

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,20 +11,28 @@ document.addEventListener("DOMContentLoaded", function() {
                alert("You clicked the submit button!"); 
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+    runGame("addition");
 });
 
 /**
  * The Main game "loop", called when the script is first loaded.
  * and after the user's answer has been processed.
  */
-function runGame(){
+function runGame(gameType) {
     // Create two random numbers between 1 and 25.
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer(){
@@ -43,8 +51,10 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";    
 }
 
 function displaySubtractQuestion(){
